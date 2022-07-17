9ab5fe483c4a8b5986acbbc5e9b87f5df6401bc4
myrin <myring100@gmail.com>
2022-07-17 01:12:57 -0700

    드디어 계산기 기능을 만들었다.
    이제 계산 기능! initCalculatr(String input) 을 global.input 과 연결후 setState로 결과값을 UI 에 표현하면 된다!!

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,78 +1,49 @@
-void main() {
-  String test = '((3+4)+3*4+3+3)';
-  List<String> testList = test.split('');
-  var holder ;
-  print(testList);
-  while(testList.contains('(')){
-    holder = trimList(testList);
+double initCalculate(String input) {
+
+  List<String> inputList = input.split('');
+  List<String> holder =[];
+  while (inputList.contains('(')) {
+    holder = trimList(inputList);
   }
 
-  print(holder.toString());
+
+  return calculate(multiplyCalculateFirst(holder));
+
+      //calculate(multiplyCalculateFirst(holder) >>>>>>>>>>>>>>>>>> 결과값 도출!!!!!!!!!!!!!
 }
 
-double initCalculate(String str) {
 
-  String result = str;
-
-  String num = '';
-  String opr = '';
-
-  int i = 0;
-  List<String> list = <String>[];
-
-  for (var element in str.runes) {
-    i++;
-    String char = String.fromCharCode(element);
-//   문자열을  list 로 쪼개서 넘긴다.
-    if (isNumeric(char) || char == '.') {
-      num = num + char;
-    } else {
-      list.add(num);
-      num = '';
-      list.add(char);
-    }
-    if (i == str.length) {
-      list.add(num);
-    }
-  }
-
-  trimList(list);
-
-
-  return calculate(multiplyCalculateFirst(list));
-}
 
 List<String> trimList(List<String> input) {
   List<String> result = input;
-  List<String> holder =[];
-
+  List<String> holder = [];
   int lastLeft = input.lastIndexOf('(');
-  int firstRight = input.indexOf(')');
-  // int firstRight = input.sublist(lastLeft).indexOf(')');
-  print(input.toString());
+  int firstRight = input.indexOf(')', lastLeft);
+  holder = input.sublist(lastLeft + 1, firstRight);
 
-  print('firstRgith ) = $firstRight');
-
-  holder = input.sublist(lastLeft+1,firstRight);
-  print('holder = $holder');
   List<String> holdingNum = [calculate(holder).toString()];
-  result.replaceRange(lastLeft, firstRight+1, holdingNum);
+  result.replaceRange(lastLeft, firstRight + 1, holdingNum);
+
   return result;
 }
 
-
 double calculate(List<String> input) {
-  if(input.length==1){
+  if (input.length == 1) {
     return double.parse(input[0]);
   }
   List<String> list = multiplyCalculateFirst(input);
   double result = 0.0;
   double a = 0;
 
+  if (list.length == 1) {
+    return double.parse(list[0]);
+  }
+
   for (int i = 0; i < list.length; i++) {
     if (i == 0) {
       a = double.parse(list[0]);
     }
+    // 연산자가 있을경우.
     if (i % 2 != 0) {
       switch (list[i]) {
         case '+':
@@ -88,35 +59,32 @@ double calculate(List<String> input) {
   return result;
 }
 
-
 List<String> multiplyCalculateFirst(List<String> input) {
   List<String> list = [];
   for (int i = 0; i < input.length; i++) {
-    if(i>0){
-      if(input[i-1]=='*'||input[i-1]=='/'||input[i-1]=='%') continue;
+    if (i > 0) {
+      if (input[i - 1] == '*' || input[i - 1] == '/' || input[i - 1] == '%')
+        continue;
     }
     if (input[i] == '*') {
       double holderingNum =
-          double.parse(list.last) * double.parse(input[i+1]);
+          double.parse(list.last) * double.parse(input[i + 1]);
       list.removeLast();
       list.add(holderingNum.toString());
     } else if (input[i] == '/') {
       double holderingNum =
-          double.parse(list.last) / double.parse(input[i+1]);
+          double.parse(list.last) / double.parse(input[i + 1]);
       list.removeLast();
       list.add(holderingNum.toString());
-
     } else if (input[i] == '%') {
       double holderingNum =
-          double.parse(list.last) % double.parse(input[i+1]);
+          double.parse(list.last) % double.parse(input[i + 1]);
       list.removeLast();
       list.add(holderingNum.toString());
-    }
-    else{
-        list.add(input[i]);
+    } else {
+      list.add(input[i]);
     }
   }
-  // print('mlultiplaycalculatefirst=  ' + list.toString());
 
   return list;
 }
