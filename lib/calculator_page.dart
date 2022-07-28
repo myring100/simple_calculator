5fe77132130e4517123fbdbcc19fe1023649d1e7
myring100 <myring100@gmail.com>
2022-07-27 20:38:34 -0700

    모든 기능을 다 넣었고 이제 테스트만 하고 버그 픽스 해야함.
    추후에  admob, firebase 연동

--- lib/calculator_page.dart
+++ lib/calculator_page.dart
@@ -5,7 +5,9 @@ import 'package:simple_calculator/resultPage.dart';
 import 'menuWidget.dart';
 import 'my-globals.dart' as global;
 import 'calculator.dart';
-import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+import 'ad_helper.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
 
 class CalculatorPage extends StatefulWidget {
   const CalculatorPage({Key? key}) : super(key: key);
@@ -15,54 +17,107 @@ class CalculatorPage extends StatefulWidget {
 }
 
 class _CalculatorPage extends State<CalculatorPage> {
+
+  BannerAd? _bannerAd;
+
+  @override
+  void initState() {
+    // TODO: Load a banner ad
+    print('initState');
+    BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      request: const AdRequest(),
+      size: AdSize.banner,
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            _bannerAd = ad as BannerAd;
+            print('banner ad set');
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          print('Failed to load a banner ad: ${err.message}');
+          ad.dispose();
+        },
+      ),
+    ).load();
+  }
+
   @override
   void setState(VoidCallback fn) {
     super.setState(fn);
   }
 
   @override
+  void dispose() {
+    // TODO: Dispose a BannerAd object
+    _bannerAd?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery
+        .of(context)
+        .size
+        .width;
+    double height = MediaQuery
+        .of(context)
+        .size
+        .height;
 
     print('width = $width Height = $height');
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Flexible(flex: 3, child: InputPage()),
-          Flexible(flex: 2, child: ResultPage()),
-          Flexible(
-              flex: 1,
-              child: MenuWidget(
-                buttonPressed: () => setState(() {
-                  try {
-                    complete();
-                  } catch (error) {
-                    print('error here!!!!!!!!!');
-                  }
-                }),
-              )),
-          Flexible(
-            flex: 6,
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(10, 15, 10, 15),
-              child: Keyboard(
-                buttonPressed: () => setState(() {
-                  try {
-                    complete();
-                    print('global result = ${global.resultString}');
-                  } catch (error) {
-                    print('error here!!!!!!!!!');
-                  }
-                }),
-              ),
+        backgroundColor: Colors.white,
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+            Flexible(flex: 2, child: InputPage()),
+        Flexible(flex: 2, child: ResultPage()),
+        Flexible(
+            flex: 1,
+            child: MenuWidget(
+              buttonPressed: () =>
+                  setState(() {
+                    try {
+                      complete();
+                    } catch (error) {
+                      print('error here!!!!!!!!!');
+                    }
+                  }),
+            )),
+        Flexible(
+          flex: 6,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(10, 15, 10, 15),
+            child: Keyboard(
+              buttonPressed: () =>
+                  setState(() {
+                    try {
+                      complete();
+                      print('global result = ${global.resultString}');
+                    } catch (error) {
+                      print('error here!!!!!!!!!');
+                    }
+                  }),
             ),
           ),
-        ],
+        ),
+        if (_bannerAd != null) Flexible(
+      flex: 1,
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+          width: _bannerAd!.size.width.toDouble(),
+          height: _bannerAd!.size.height.toDouble(),
+          child: AdWidget(ad: _bannerAd!),
+        ),
       ),
+    )
+    ],
+    )
+    ,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'calculator_page.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  Future<InitializationStatus> _initGoogleMobileAds() {
+    // TODO: Initialize Google Mobile Ads SDK
+    return MobileAds.instance.initialize();
+  }
 
   @override
   Widget build(BuildContext context) {
